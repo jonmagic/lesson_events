4b660088f919194be256752c69c75d1af3cc5e5b
Jonathan Hoyt <hoyt@github.com>
2013-04-21 00:38:02 -0700

    Wire up Fixtures and make homepage content dynamic.

--- assets/javascripts/app.coffee
+++ assets/javascripts/app.coffee
@@ -2,4 +2,29 @@
 
 window.App = Ember.Application.create()
 
+App.Store = DS.Store.extend
+  revision: 12
+  adapter: "DS.FixtureAdapter"
+
 App.Router.map ->
+  @resource "lesson", { path: "lesson/:lesson_id"}
+
+App.IndexRoute = Ember.Route.extend
+  model: ->
+    App.Lesson.find(1)
+  setupController: (controller, model) ->
+    controller.set "upcomingEvent", model
+
+App.Lesson = DS.Model.extend
+  title: DS.attr("string")
+  summary: DS.attr("string")
+  imageUrl: DS.attr("string")
+
+App.Lesson.FIXTURES = [
+  {
+    id: 1
+    title: "Animations in Javascript"
+    summary: "Build animations in the web browser using javascript and the D3.js library."
+    imageUrl: "/assets/images/animations_in_javascript.jpg"
+  }
+]
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -3,6 +3,39 @@
 
   window.App = Ember.Application.create();
 
-  App.Router.map(function() {});
+  App.Store = DS.Store.extend({
+    revision: 12,
+    adapter: "DS.FixtureAdapter"
+  });
+
+  App.Router.map(function() {
+    return this.resource("lesson", {
+      path: "lesson/:lesson_id"
+    });
+  });
+
+  App.IndexRoute = Ember.Route.extend({
+    model: function() {
+      return App.Lesson.find(1);
+    },
+    setupController: function(controller, model) {
+      return controller.set("upcomingEvent", model);
+    }
+  });
+
+  App.Lesson = DS.Model.extend({
+    title: DS.attr("string"),
+    summary: DS.attr("string"),
+    imageUrl: DS.attr("string")
+  });
+
+  App.Lesson.FIXTURES = [
+    {
+      id: 1,
+      title: "Animations in Javascript",
+      summary: "Build animations in the web browser using javascript and the D3.js library.",
+      imageUrl: "/assets/images/animations_in_javascript.jpg"
+    }
+  ];
 
 }).call(this);
